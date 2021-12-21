89f5f936f7fe747d170fd0ed68fd213b641469f7
ray <1552937000@qq.com>
2021-12-22 00:34:01 +0800

    update server register

--- server.go
+++ server.go
@@ -3,12 +3,12 @@ package geerpc
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"gee-rpc/codec"
 	"io"
 	"log"
 	"net"
 	"reflect"
+	"strings"
 	"sync"
 )
 
@@ -98,6 +98,8 @@ func (server *Server) serveCodec(cc codec.Codec) {
 type request struct {
 	h          *codec.Header
 	arg, reply reflect.Value
+	mType      *methodType
+	svc        *service
 }
 
 func (server *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
@@ -117,9 +119,19 @@ func (server *Server) readRequest(cc codec.Codec) (*request, error) {
 		return nil, err
 	}
 	req := &request{h: h}
-	// TODO: just suppose it's string
-	req.arg = reflect.New(reflect.TypeOf(""))
-	if err = cc.ReadBody(req.arg.Interface()); err != nil {
+	req.svc, req.mType, err = server.findService(h.ServiceMethod)
+	if err != nil {
+		return req, err
+	}
+	req.arg = req.mType.newArgv()
+	req.reply = req.mType.newReply()
+
+	// make sure that argI is a pointer, ReadBody need a pointer as parameter
+	argI := req.arg.Interface()
+	if req.arg.Type().Kind() != reflect.Ptr {
+		argI = req.arg.Addr().Interface()
+	}
+	if err = cc.ReadBody(argI); err != nil {
 		log.Println("rpc server: read arg err: ", err)
 	}
 	return req, nil
@@ -134,10 +146,15 @@ func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interfa
 }
 
 func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
-	// TODO: just print arg and send a hello message
 	defer wg.Done()
-	log.Println(req.h, req.arg.Elem())
-	req.reply = reflect.ValueOf(fmt.Sprintf("geerpc rsp %d", req.h.Seq))
+	err := req.svc.call(req.mType, req.arg, req.reply)
+	if err != nil {
+		req.h.Error = err.Error()
+		server.sendResponse(cc, req.h, invalidRequest, sending)
+		return
+	}
+	// log.Println(req.h, req.arg.Elem())
+	// req.reply = reflect.ValueOf(fmt.Sprintf("geerpc rsp %d", req.h.Seq))
 	server.sendResponse(cc, req.h, req.reply.Interface(), sending)
 }
 
@@ -152,3 +169,24 @@ func (server *Server) Register(rcvr interface{}) error {
 
 // Register publishes the receiver's methods in the DefaultServer.
 func Register(rcvr interface{}) error { return DefaultServer.Register(rcvr) }
+
+// 通过服务名再找到方法
+func (server *Server) findService(serviceMethod string) (svc *service, mType *methodType, err error) {
+	dot := strings.LastIndex(serviceMethod, ".")
+	if dot < 0 {
+		err = errors.New("rpc server: service/method request ill-formed: " + serviceMethod)
+		return
+	}
+	serviceName, methodName := serviceMethod[:dot], serviceMethod[dot+1:]
+	svcI, ok := server.serviceMap.Load(serviceName)
+	if !ok {
+		err = errors.New("rpc server: can't find service " + serviceName)
+		return
+	}
+	svc = svcI.(*service)
+	mType = svc.method[methodName]
+	if mType == nil {
+		err = errors.New("rpc server: can't find method " + methodName)
+	}
+	return
+}
